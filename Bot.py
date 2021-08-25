2cfea4cf419a33fdcabdbc20c270f2c88b85b660
olliym <89498648+olliym@users.noreply.github.com>
2021-08-25 05:47:24 +0300

    qqqq

--- Bot.py
+++ Bot.py
@@ -149,7 +149,7 @@ def loop6():
 
 print("Author: https://github.com/NoNameoN-A")
 
-vidUrl = "https://www.tiktok.com/@github_nonameon/video/6898692248968400130" #Change it
+vidUrl = "https://www.tiktok.com/@kareykadashaw/video/6982132494992690434" #Change it
 
 bot = int(input("What do you want to do?\n1 - Auto views(500)\n2 - Auto hearts\n3 - Auto (FIRST) comments heart\n4 - Auto followers\n5 - Auto Share\n6 - Simple reload\n"))
 i = 0
@@ -158,7 +158,7 @@ chrome_options = webdriver.ChromeOptions()
 #chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome(executable_path=r'/home/nonameon/Desktop/Programmi/TikTok-Follow-Heart-Views-Bot-main/chromedriver',chrome_options=chrome_options) #Change it
+driver = webdriver.Chrome(executable_path=r'\C:\Users\maska\Saved Games\Desktop\Tik-Tok-Views-Bot-main\TikTok-Follow-Heart-Views-Bot-main/chromedriver.exe',chrome_options=chrome_options) #Change it
 
 driver.get("https://vipto.de/")
 
